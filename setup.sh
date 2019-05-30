57885a5dd0f74a6db97e1ce9b21f79c56d803db9
Ryota Misumi <deltama37@users.noreply.github.com>
2019-05-30 10:55:34 +0900

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 workdir=$(pwd)
 
-# ファイルが存在する場合は削除する場合は一度ファイルをどうするか確認させる
+# TODO:ファイルが存在する場合は削除する場合は一度ファイルをどうするか確認させる
 
 for file in $(git ls-files | cut -d/ -f1 | uniq | grep -v setup.sh); do
-    # シンボリックリンクが存在する場合はシンボリックリンクを削除する
-    # osごとの設定ファイルはos次第にする
+    # TODO:シンボリックリンクが存在する場合はシンボリックリンクを削除する
+    # TODO:osごとの設定ファイルはos次第にする
     if [ -e $HOME/$file ]; then
         rm $HOME/$file
     fi
